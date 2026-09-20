97dc4cfd7a08c6916903604d6b22aaff3ec06b90
Technoblade1889 <Technoblade1889@users.noreply.github.com>
2026-09-20 20:08:32 +0800

    生成 v15 快照存档页

--- build-versions.js
+++ build-versions.js
@@ -33,6 +33,7 @@ const VERSIONS = [
   { v: "v12", commit: "dbc4113" }, // UI 优化
   { v: "v13", commit: "9625f65" }, // 逐日计划全年 12 个月
   { v: "v14", commit: "40456f3" }, // 去 JS、TS 顺带学语法
+  { v: "v15", commit: "eebec03" }, // 六级每日任务加作文/翻译/精听
 ];
 
 function show(commit, file) {
